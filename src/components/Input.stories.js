5edafdaccd32d081fc131f743299ea55d684a3dd
James Lave <30740613+jlave-dev@users.noreply.github.com>
2020-07-16 02:29:50 -0500

    🔥 Get rid of unused files

--- src/components/Input.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import Input from './Input';
-
-export default {
-  title: 'Input',
-};
-
-export const text = () => <Input onTextComplete={action('textComplete')} />;
