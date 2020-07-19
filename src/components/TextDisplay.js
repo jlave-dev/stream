19e2ef371e1b8bad09670507c73d9e5911717fcc
James Lave <30740613+jlave-dev@users.noreply.github.com>
2020-07-19 09:32:39 -0500

    🚧 Add Save and Clear buttons to TextDisplay

--- src/components/TextDisplay.js
+++ src/components/TextDisplay.js
@@ -1,25 +1,70 @@
 import React, { useState } from 'react';
-import { string } from 'prop-types';
+import { string, func } from 'prop-types';
 import './TextDisplay.scss';
 
-const TextDisplay = ({ text }) => {
+const TextDisplay = ({ onClear, text }) => {
   const [isBlurred, setIsBlurred] = useState(true);
+
+  function handleClear() {
+    // eslint-disable-next-line no-alert
+    if (window.confirm('Are you sure you want to clear the text?')) {
+      onClear();
+    }
+  }
 
   return (
     <div className="TextDisplay content">
-      <button
-        className="button"
-        type="button"
-        onClick={() => {
-          setIsBlurred(!isBlurred);
-        }}
-      >
-        {isBlurred ? 'Show' : 'Hide'} text
-      </button>
+      {text && (
+        <div className="level">
+          <div className="level-left">
+            <div className="level-item">
+              <button
+                className="button is-white"
+                type="button"
+                title="Toggle text visibility"
+                onClick={() => {
+                  setIsBlurred(!isBlurred);
+                }}
+              >
+                <span className="icon">
+                  <i
+                    className={`fas ${isBlurred ? 'fa-eye-slash' : 'fa-eye'}`}
+                  />
+                </span>
+              </button>
+              <button
+                className="button is-white"
+                type="button"
+                title="Save text"
+                onClick={() => {}}
+              >
+                <span className="icon">
+                  <i className="fas fa-save" />
+                </span>
+              </button>
+            </div>
+          </div>
+
+          <div className="level-right">
+            <div className="level-item">
+              <button
+                className="button is-white"
+                type="button"
+                title="Clear text"
+                onClick={handleClear}
+              >
+                <span className="icon">
+                  <i className="fas fa-trash" />
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
 
       <div className={isBlurred ? 'is-blurred' : ''}>
         {text.split('\n').map((line) => (
-          <p key={line} className="has-text-centered">
+          <p key={line} style={{ textAlign: 'start' }}>
             {line}
           </p>
         ))}
@@ -29,6 +74,7 @@ const TextDisplay = ({ text }) => {
 };
 
 TextDisplay.propTypes = {
+  onClear: func.isRequired,
   text: string.isRequired,
 };
 
